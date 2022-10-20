764af941ad1299baedcb828d720f4e9aeb7b2018
Ben <ben250402@yahoo.co.uk>
2022-10-20 15:03:34 +0100

    BulletSpawner created that fires bullets at every X seconds
    Rotation of spawner also effects which direction bullets go

--- Assets/Code/BulletSpawner.cs
+++ Assets/Code/BulletSpawner.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class BulletSpawner : MonoBehaviour
 {
     public float delayBetweenShoot = 1.0f;
+    public GameObject bullet;
+    private float timer = 0;
+    private Vector3 location;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        location = transform.position;
+
+        timer += Time.deltaTime;
+
+        if (timer >= delayBetweenShoot)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+
+        GameObject newBullet = Instantiate(bullet, location, Quaternion.identity);
+        newBullet.transform.rotation = transform.rotation;
+        timer = 0;
     }
 }
