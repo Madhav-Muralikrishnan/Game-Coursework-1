f4a15c2fc0a208c4a02abbbfa7d5e77728dbc32d
Ben <ben250402@yahoo.co.uk>
2022-10-27 11:51:40 +0100

    Changed bullet movement to use rigidbody
    Added skeleton for collisions and bounces

--- Assets/Code/BulletCollision.cs
+++ Assets/Code/BulletCollision.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class BulletCollision : MonoBehaviour
 {
+	public int numCollisions;
+	public int maxCollisions;
 	private void OnCollisionEnter(Collision collision)
 	{
+		if (collision.gameObject.tag == "Player")
+		{
+			Debug.LogWarning("Die");
+		}
+		var movement = GetComponent<BulletMovement>();
+		if (numCollisions < maxCollisions)
+		{			
+			if (collision.gameObject.tag == "Wall")
+			{
+				movement?.CollideWithWall();
+			}
+			else if (collision.gameObject.tag == "Floor")
+			{
+				movement?.CollideWithFloor();
+			}
+			numCollisions++;
+			return;
+		}
 		gameObject.SetActive(false);
 	}
 }
