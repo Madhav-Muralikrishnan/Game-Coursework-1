af2a9b1d3c9d4fd2b1b13f59ee3b30a86437ac8f
Ben <ben250402@yahoo.co.uk>
2022-11-02 12:05:42 +0000

    Heartbeats now show on UI
    Plus goes faster when in slow mo

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public Player player;
+    public Text heartbeatText;
     public float slowMoTimer;
     public float slowMoTimerMax = 2;
     public float regenSlowMoSpeed = 0.5f;
@@ -13,7 +15,9 @@ public class GameController : MonoBehaviour
     private Vector3 lastCheckPointRotation;
     private int numDeaths = 0;
     private float timer = 0;
-    private int seconds = 0;
+    private float timerWhenSlowMultiplier = 1;
+    private int totalHeartBeats = 0;
+    private int prevHeartBeats = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -26,7 +30,20 @@ public class GameController : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        seconds = (int) timer % 60;
+
+        if (isSlowMo)
+            timer += Time.deltaTime * timerWhenSlowMultiplier;
+
+        totalHeartBeats = (int) timer % 60;
+
+        if (prevHeartBeats < totalHeartBeats)
+        {
+            //Play heartbeat audio
+        }
+
+        prevHeartBeats = totalHeartBeats;
+
+        heartbeatText.text = totalHeartBeats.ToString();
     }
 
     public void Respawn()
