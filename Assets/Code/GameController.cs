2a944575720f686f8991c66778fae60ae4b3aa0c
Ben <ben250402@yahoo.co.uk>
2022-11-06 21:43:19 +0000

    Changed spaces to tabs

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -5,98 +5,98 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public Player player;
-    public GameObject door1;
-    public GameObject door2;
-    public GameObject slowMoBar;
-    public AudioSource heartBeatSound;
-    public AudioSource doorSound;
-    public Text heartbeatText;
-    public float slowMoTimer;
-    public float slowMoTimerMax = 2;
-    public float regenSlowMoSpeed = 0.5f;
-    public bool isSlowMo = false;
-    public bool powerUp2Active = false;
-    public bool leftClick = false;
+	public Player player;
+	public GameObject door1;
+	public GameObject door2;
+	public GameObject slowMoBar;
+	public AudioSource heartBeatSound;
+	public AudioSource doorSound;
+	public Text heartbeatText;
+	public float slowMoTimer;
+	public float slowMoTimerMax = 2;
+	public float regenSlowMoSpeed = 0.5f;
+	public bool isSlowMo = false;
+	public bool powerUp2Active = false;
+	public bool leftClick = false;
 
-    private Vector3 lastCheckPoint;
-    private Vector3 lastCheckPointRotation;
-    private int numDeaths = 0;
-    private float timer = 0;
-    private float timerWhenSlowMultiplier = 1;
-    private int totalHeartBeats = 0;
-    private int key2sCollected = 0;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        slowMoTimer = slowMoTimerMax;
-        SetCheckpoint(new Vector3(-5,1,-7), new Vector3(0,0,0));
-    }
+	private Vector3 lastCheckPoint;
+	private Vector3 lastCheckPointRotation;
+	private int numDeaths = 0;
+	private float timer = 0;
+	private float timerWhenSlowMultiplier = 1;
+	private int totalHeartBeats = 0;
+	private int key2sCollected = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer += Time.deltaTime;
+	// Start is called before the first frame update
+	void Start()
+	{
+		slowMoTimer = slowMoTimerMax;
+		SetCheckpoint(new Vector3(-5,1,-7), new Vector3(0,0,0));
+	}
 
-        if (isSlowMo)
-            timer += Time.deltaTime * timerWhenSlowMultiplier;
+	// Update is called once per frame
+	void Update()
+	{
+		timer += Time.deltaTime;
 
-        if (timer >= 1)
-        {
-            totalHeartBeats++;
-            timer = 0;
+		if (isSlowMo)
+			timer += Time.deltaTime * timerWhenSlowMultiplier;
 
-            //Play heartbeat audio
-            heartBeatSound.Play();
+		if (timer >= 1)
+		{
+			totalHeartBeats++;
+			timer = 0;
 
-            heartbeatText.text = totalHeartBeats.ToString();
-        }
+			//Play heartbeat audio
+			heartBeatSound.Play();
 
-    }
+			heartbeatText.text = totalHeartBeats.ToString();
+		}
 
-    public void Respawn()
-    {
-        Debug.Log("Respawning to " + lastCheckPoint);
-        player.transform.position = lastCheckPoint;
-        player.transform.eulerAngles = lastCheckPointRotation;
-    }
+	}
 
-    public void SetCheckpoint(Vector3 checkPointPosition, Vector3 checkPointRotation)
-    {
-        lastCheckPoint = checkPointPosition;
-        lastCheckPointRotation = checkPointRotation;
-    }
+	public void Respawn()
+	{
+		Debug.Log("Respawning to " + lastCheckPoint);
+		player.transform.position = lastCheckPoint;
+		player.transform.eulerAngles = lastCheckPointRotation;
+	}
 
-    public void AddToDeathCounter()
-    {
-        numDeaths++;
-    }
+	public void SetCheckpoint(Vector3 checkPointPosition, Vector3 checkPointRotation)
+	{
+		lastCheckPoint = checkPointPosition;
+		lastCheckPointRotation = checkPointRotation;
+	}
 
-    public void Finish()
-    {
-        Debug.Log("Finish");
-    }
+	public void AddToDeathCounter()
+	{
+		numDeaths++;
+	}
 
-    public void Key1()
-    {
-        door1.SetActive(false);
-        doorSound.Play();
-    }
+	public void Finish()
+	{
+		Debug.Log("Finish");
+	}
 
-    public void Key2()
-    {
-        key2sCollected++;
+	public void Key1()
+	{
+		door1.SetActive(false);
+		doorSound.Play();
+	}
 
-        if (key2sCollected >= 4)
-        {
-            door2.SetActive(false);
-            doorSound.Play();
-        }
-    }
+	public void Key2()
+	{
+		key2sCollected++;
 
-    public void ActivateSlowMoBar()
-    {
-        slowMoBar.SetActive(true);
-    }
+		if (key2sCollected >= 4)
+		{
+			door2.SetActive(false);
+			doorSound.Play();
+		}
+	}
+
+	public void ActivateSlowMoBar()
+	{
+		slowMoBar.SetActive(true);
+	}
 }
