0dc0fb2f63ebc68f142a7f87edc308fe8e5c277a
Madhavius <madhavmuralikrishnan@gmail.com>
2022-11-20 23:10:11 +0000

    Made cursor disappear

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -24,6 +24,7 @@ public class Player : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+		Cursor.visible = false;
 		gameController = FindObjectOfType<GameController>();
 		rigidBody = GetComponent<Rigidbody>();
 	}
